bd5b9d25a731b0f1276f3010d4994b736d7cb376
Graph-Donte-Crypto <Dimasik841@gmail.com>
2020-03-14 19:25:20 +0300

    Created struct Xy - 2D point; Created trait XyProvider for Xy, f32, f64, i32, i64; Created struct Movable for movable objects

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,126 @@ use ggez::{event, graphics, Context, GameResult};
 use ggez::event::{KeyCode, KeyMods};
 
 use std::f32::consts::PI;
+use ggez::mint::Point2;
+use ggez::input::mouse::MouseButton;
+use core::ops;
 
 mod conf {
     pub const FPS: u32 = 60;
     pub const SCREEN_SIZE: (f32, f32) = (800.0, 600.0);
 }
 
+#[derive(Clone, Copy)]
+pub struct Xy {
+    pub x : f64,
+    pub y : f64
+}
+
+pub trait XyProvider {
+    fn get_x(&self) -> f64;
+    fn get_y(&self) -> f64;
+}
+
+impl From<Xy> for Point2<f32> {
+	fn from(xy : Xy) -> Self {
+		return Point2 { x : xy.x as f32, y: xy.y as f32};
+	}
+}
+
+impl XyProvider for Xy {
+    fn get_x(&self) -> f64 { return self.x; }
+    fn get_y(&self) -> f64 { return self.y; }
+}
+
+impl XyProvider for f64 {
+    fn get_x(&self) -> f64 { return *self; }
+    fn get_y(&self) -> f64 { return *self; }
+}
+
+impl XyProvider for f32 {
+    fn get_x(&self) -> f64 { return *self as f64; }
+    fn get_y(&self) -> f64 { return *self as f64; }
+}
+
+impl XyProvider for i32 {
+    fn get_x(&self) -> f64 { return *self as f64; }
+    fn get_y(&self) -> f64 { return *self as f64; }
+}
+
+impl XyProvider for i64 {
+    fn get_x(&self) -> f64 { return *self as f64; }
+    fn get_y(&self) -> f64 { return *self as f64; }
+}
+
+pub fn Xy<T : XyProvider, R : XyProvider>(tx : T, ty : R) -> Xy {
+	return Xy::new(tx, ty);
+}
+
+impl Xy {
+    fn new<T : XyProvider, R : XyProvider>(tx : T, ty : R) -> Self {
+        return Xy { x: tx.get_x(), y: ty.get_y() }
+    }
+	fn set<T : XyProvider, R : XyProvider>(&mut self, tx : T, ty : R) -> & Self {
+		self.x = tx.get_x();
+		self.y = ty.get_y();
+		return self;
+	}
+    // ...
+}
+
+impl ops::Add<Xy> for Xy {
+	type Output = Xy;
+	fn add(self, xy: Xy) -> Self {
+		return Xy(self.x + xy.x, self.y + xy.x);
+	}
+}
+impl ops::Sub<Xy> for Xy {
+	type Output = Xy;
+	fn sub(self, xy: Xy) -> Self {
+		return Xy(self.x - xy.x, self.y - xy.x);
+	}
+}
+impl ops::AddAssign<Xy> for Xy {
+	fn add_assign(&mut self, xy: Xy) {
+		self.x += xy.x;
+		self.y += xy.y;
+	}
+}
+
+impl ops::MulAssign<f64> for Xy {
+	fn mul_assign(&mut self, coefficient: f64) {
+		self.x *= coefficient;
+		self.y *= coefficient;
+	}
+}
+
+struct Movable {
+	coordinates : Xy,
+	acceleration_vector : Xy,
+	movement_vector : Xy,
+	braking_coefficient : f64, //the more the faster brakes are: [0, 1] -> 0 - no braking, 1 - no movement
+}
+
+impl Movable {
+	fn move0(&mut self) -> & Self {
+		self.movement_vector += self.acceleration_vector;
+		self.movement_vector *= 1 as f64 - self.braking_coefficient;
+		self.coordinates += self.movement_vector;
+		return self;
+	}
+}
+
 struct State {
     // Here we will find the map, player, etc.
+	coordinates_mouse : Xy,
+	object : Movable,
 }
 
 impl ggez::event::EventHandler for State {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         while ggez::timer::check_update_time(ctx, conf::FPS) {
+			//self.object.coordinates.x += 1 as f64;
+			self.object.move0();
             // Here, items from State should be updated.
             // It's good when ech item (player, entities) have theri update() function.
         }
@@ -29,30 +136,27 @@ impl ggez::event::EventHandler for State {
         // You can access State fields by `this.field`
 
         // Example of drawing a rectangle.
-        let rectangle = graphics::Mesh::new_rectangle(      // create a __MESH__ 
-                ctx,
-                graphics::DrawMode::fill(),                 // is filled
-                graphics::Rect::new_i32(10, 10, 20, 20),    // Mesh is a rectangle; coords of corners
-                                                            // (zero-point is a center of rect:
-                                                            //  x, y, width, height)
-                [0.0, 0.0, 0.0, 1.0].into(),                // RGBA color
-            )?;
+		let circle = graphics::Mesh::new_circle(
+			ctx,
+			graphics::DrawMode::fill(),
+			Xy(0, 0),
+			10 as f32,
+			0.25 as f32,
+			[1.0, 1.0, 1.0, 1.0].into()
 
-        let draw_params = graphics::DrawParam::new()        // This describes the __POSITION__ of created mesh
-                .dest([50.0, 60.0])                         // coordinates of center on screen
-                .rotation(PI / 6.0)                           // rotation around the center of the mesh
-                .scale([1.5, 2.0]);
+		)?;
+		let draw_params_circle = graphics::DrawParam::new()        // This describes the __POSITION__ of created mesh
+			.dest(self.object.coordinates);
 
-        graphics::draw(ctx, &rectangle, draw_params)?;      // Draw the resulting mesh on the screen
+		graphics::draw(ctx, &circle, draw_params_circle)?;
 
-
-        graphics::present(ctx)?;                            // Something that helps timer to work
+		graphics::present(ctx)?;                            // Something that helps timer to work
         ggez::timer::yield_now();                           // ...nevermind
         return Ok(());
     }
 
 
-    fn key_up_event( &mut self, ctx: &mut Context, keycode: KeyCode, _keymod: KeyMods) {
+    fn key_up_event( &mut self, _ctx: &mut Context, keycode: KeyCode, _keymod: KeyMods) {
         // There are also other kyeboard and mouse functions, see:
         // https://docs.rs/ggez/0.5.1/ggez/event/trait.EventHandler.html
         match keycode {
@@ -60,7 +164,15 @@ impl ggez::event::EventHandler for State {
             _ => (),
         }
     }
+	fn mouse_button_down_event(&mut self, _ctx: &mut Context, _button: MouseButton, _x: f32, _y: f32) {
+
+	}
+	fn mouse_motion_event(&mut self, _ctx: &mut Context, x: f32, y: f32, _dx: f32, _dy: f32) {
+		self.coordinates_mouse = Xy(x, y);
+	}
 }
+
+
 
 fn main() -> GameResult {
     // For drawing we are using 'ggez' __library__, not engine :)
@@ -72,7 +184,15 @@ fn main() -> GameResult {
         .build()?;
 
     // create the world (can be loaded from file or whatever)
-    let mut state = State {  };
+    let mut state = State {
+		coordinates_mouse: Xy::new(0, 0),
+		object: Movable {
+			coordinates: Xy(100, 100),
+			acceleration_vector: Xy(0, 0),
+			movement_vector: Xy(5, 0),
+			braking_coefficient : 0.01
+		}
+	};
 
     return event::run(ctx, events_loop, &mut state);
 }
